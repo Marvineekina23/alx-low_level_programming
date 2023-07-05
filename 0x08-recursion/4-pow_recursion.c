cbadbea84d996f49b17b88212930e4e425007498
Marvineekina23 <arafatekina23@gmail.com>
2023-07-05 00:02:18 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,30 +1,17 @@
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string
- * @accept: bytes to search
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *_strpbrk(char *s, char *accept)
+int _pow_recursion(int x, int y)
 {
-	int l, l2, i, j;
-
-	l = 0, l2 = 0;
-	while (s[l] != '\0')
-		l++;
-	while (accept[l2] != '\0')
-		l2++;
-	for (i = 0; i < l; i++, s++)
-	{
-		for (j = 0; j < l2; j++)
-		{
-			if (*s == *(accept + j))
-			{
-				return (s);
-			}
-		}
-	}
-	return (0);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
