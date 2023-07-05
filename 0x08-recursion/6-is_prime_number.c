b372cf2f4abd0967fac2cc3e57cd03f1a6d807c6
Marvineekina23 <arafatekina23@gmail.com>
2023-07-05 00:13:01 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
- * is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
- * @n: number for input
- * Return: 0
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int is_prime_number(int n)
 {
-	return (helper_prime(n, 2, n / 2));
-}
-
-/**
- *  helper_prime - blank
- * @n: checkl
- * @i: for the increment
- * @limit: stop
- * Return: 0
- */
-
-int helper_prime(int n, int i, int limit)
-{
-	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
+	if (n <= 1)
 		return (0);
-	else if (n % i != 0 && i <= limit)
-		return (helper_prime(n, i + 1, limit));
-	else
-		return (1);
+	return (prime2(n, 2));
 }
