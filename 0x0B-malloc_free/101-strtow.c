f28de939595df98ecdbcaad6ade99cf8ce6c9f01
Marvineekina23 <arafatekina23@gmail.com>
2023-07-09 23:11:49 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,83 +1,39 @@
+#include "notrebloh.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+ * *_realloc - reallocate memory size function
+ * @ptr: pointer to address of old memory location
+ * @old_size: unsigned int type of old memory size
+ * @new_size: unsigned int type for new memory size
+ * Return:  return pointer to array
+ */
 
-int number(char *str)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int a, num = 0;
+	char *s;
 
-	for (a = 0; str[a] != '\0'; a++)
+	if (new_size > old_size)
 	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
-		}
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
 	}
-	return num;
-}
-
-void free_everything(char **string, int i)
-{
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
-}
-
-char **strtow(char *str)
-{
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
-
-	if (str == NULL || *str == '\0')
-		return NULL;
-
-	total_words = number(str);
-
-	if (total_words == 0)
-		return NULL;
-
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == NULL)
-		return NULL;
-
-	for (; *str != '\0' && b < total_words;)
+	if (new_size == old_size)
 	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == NULL)
-			{
-				free_everything(words, b);
-				return NULL;
-			}
-
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-
-			b++;
-			c = 0;
-			length = 0;
-			str++;
-		}
+		return (ptr);
 	}
-
-	return words;
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 }
